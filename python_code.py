03cfc425e179c3417769e32786dadc7bacb65034
ADITYA KUMAR <adityakumar529@gmail.com>
2020-04-02 12:32:03 +0530

    Create python_code.py

---
+++ python_code.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ##this notebook will be mainly used for the capstone project
+# 
+
+# In[2]:
+
+
+import pandas as py
+
+
+# In[3]:
+
+
+import numpy as np
+
+
+# In[6]:
+
+
+print ("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
